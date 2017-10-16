e891865f8b18b5d4e9fe888686bb280253ad2e99
yanshu <huangfeifei91@gmail.com>
2017-10-16 12:52:00 -0400

    use no_reco and bug fix

--- pisa/analysis/distribution_maker_nutau.py
+++ pisa/analysis/distribution_maker_nutau.py
@@ -4,7 +4,7 @@ import copy
 
 class DistributionMakerNutau(DistributionMaker):
 
-    def get_variables(self, variables, apply_sys_to_mc=True, return_stages=['mc','icc'], pid_selection=''):
+    def get_variables(self, variables, no_reco=False, apply_sys_to_mc=True, return_stages=['mc','icc'], pid_selection=''):
         # Note: only works with mc, icc and/or data stages
 
         #print "apply_sys_to_mc ", apply_sys_to_mc
@@ -36,35 +36,39 @@ class DistributionMakerNutau(DistributionMaker):
                 variables.append('pid')
 
         # params for data
-        if data_stage != None and 'data' in return_stages:
-            data_params = data_stage.get_fields(fields=variables)
+        if data_stage is not None and 'data' in return_stages:
+            data_params = data_stage.get_fields(fields=variables, no_reco=no_reco)
         else:
             data_params = None
 
         # params for icc 
-        if icc_stage != None and 'icc' in return_stages:
-            icc_params = icc_stage.get_fields(fields=variables, icc_file_name = icc_stage.params.icc_bg_file.value)
+        if icc_stage is not None and 'icc' in return_stages:
+            icc_params = icc_stage.get_fields(fields=variables, no_reco=no_reco, icc_file_name = icc_stage.params.icc_bg_file.value)
         else:
             icc_params = None
 
         # params for mc 
-        if mc_stage != None and 'mc' in return_stages:
+        if mc_stage is not None and 'mc' in return_stages:
             mc_variables=copy.deepcopy(variables)
-            for param in ['weight', 'sumw2', 'reco_energy', 'reco_coszen', 'pid']:
+            if no_reco==False:
                 # weight and sumw2 will be used in plotting script, 'reco_energy', 'reco_coszen', 'pid' are the binning 
+                mc_variables_add=['weight', 'sumw2', 'reco_energy', 'reco_coszen', 'pid']
+            else:
+                mc_variables_add=['weight', 'sumw2']
+            #for param in ['weight', 'sumw2', 'reco_energy', 'reco_coszen', 'pid']:
+            for param in mc_variables_add:
                 if param not in mc_variables:
                     mc_variables.append(param)
-            mc_params = mc_stage.get_fields(fields=mc_variables)
+            mc_params = mc_stage.get_fields(fields=mc_variables, no_reco=no_reco)
             if apply_sys_to_mc:
-                if combine_stage !=None:
+                if combine_stage is not None:
                     transforms = combine_stage.get_transforms()
+                    print "applying transforms in combine_stage"
                     for i in range(0, len(transforms)):
                         transform = transforms[i]
                         input_names = transform.input_names
                         output_name = transform.output_name
                         transform_array = transform.xform_array
-                        #print "shape transform_array", np.shape(transform_array)
-                        #print "input_names", input_names
                         for idx,flav in enumerate(input_names):
                             bin_idx=[]
                             for idx2, (bin_name, bin_edges) in enumerate(zip(transform.output_binning.names, transform.output_binning.bin_edges)):
@@ -74,27 +78,17 @@ class DistributionMakerNutau(DistributionMaker):
                                 bin_idx.append(digitized_idx)
                             transform_in_bin = np.array([transform_array[idx][i][j][k] for i,j,k in zip(bin_idx[0], bin_idx[1], bin_idx[2])])
                             mc_params[flav]['weight']*= transform_in_bin
-                if sys_stage != None:
+                if sys_stage is not None:
                     transforms = sys_stage.get_transforms()
-                    #print "In sys stage, len transforms", len(transforms)
+                    print "applying transforms in sys_stage"
+                    combine_groups = combine_stage.combine_groups
                     for i in range(0, len(transforms)):
                         transform = transforms[i]
                         input_names = transform.input_names
                         output_name = transform.output_name
                         transform_array = transform.xform_array
-                        #print "     shape transform_array", np.shape(transform_array)
-                        #print "     sys in input_names: ", input_names
-                        #print "     sys in output_names: ", output_name
                         for idx, input_flav in enumerate(input_names):
-                            #print "idx, input_flav", idx, " ", input_flav
-                            if 'nc' in input_flav:
-                                flavs_to_apply=['numu_nc', 'nutau_nc', 'nue_nc', 'numubar_nc', 'nutaubar_nc', 'nuebar_nc']
-                            if input_flav=='nue_cc':
-                                flavs_to_apply=['nue_cc', 'nuebar_cc']
-                            if input_flav=='numu_cc':
-                                flavs_to_apply=['numu_cc', 'numubar_cc']
-                            if input_flav=='nutau_cc':
-                                flavs_to_apply=['nutau_cc', 'nutaubar_cc']
+                            flavs_to_apply = combine_groups[input_flav]
                             for flav in flavs_to_apply:
                                 bin_idx = []
                                 for idx2, (bin_name, bin_edges) in enumerate(zip(transform.output_binning.names, transform.output_binning.bin_edges)):
@@ -103,13 +97,13 @@ class DistributionMakerNutau(DistributionMaker):
                                     digitized_idx -= 1
                                     bin_idx.append(digitized_idx)
                                 transform_in_bin = np.array([transform_array[i][j][k] for i,j,k in zip(bin_idx[0], bin_idx[1], bin_idx[2])])
-                                #print "len transform_in_bin", len(transform_in_bin)
                                 mc_params[flav]['weight']*= transform_in_bin
         else:
             mc_params = None
+        tol=0
 
         if pid_selection!='':
-            if data_params!=None:
+            if data_params is not None:
                 pid_copy = copy.deepcopy(data_params['pid'])
                 if pid_selection=='cscd':
                     data_cut = np.logical_and(pid_copy>=-3, pid_copy<2.0)
@@ -117,7 +111,7 @@ class DistributionMakerNutau(DistributionMaker):
                     data_cut = pid_copy>=2.0
                 for key in data_params.keys():
                     data_params[key] = data_params[key][data_cut]
-            if icc_params!=None:
+            if icc_params is not None:
                 pid_copy = copy.deepcopy(icc_params['pid'])
                 if pid_selection=='cscd':
                     icc_cut = np.logical_and(pid_copy>=-3, pid_copy<2.0)
@@ -127,7 +121,7 @@ class DistributionMakerNutau(DistributionMaker):
                     if key=='weight':
                         continue
                     icc_params[key] = icc_params[key][icc_cut]
-            if mc_params!=None:
+            if mc_params is not None:
                 for flav in mc_params.keys():
                     pid_copy = copy.deepcopy(mc_params[flav]['pid'])
                     if pid_selection=='cscd':
