2197000af283033bb81053ffe2621085e2b0489f
Philipp Eller <pde3@psu.edu>
2017-10-18 16:56:26 -0400

    vectorize

--- pisa/stages/osc/propy/osc.py
+++ pisa/stages/osc/propy/osc.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numba import jit
+
 elec=0
 muon=1
 tau=2
@@ -6,57 +8,58 @@ tau=2
 tworttwoGf = 1.52588e-4
 LoEfac = 2.534
 
-# Calculate vacuum Hamiltonian in flavor basis for neutrino or 
-# antineutrino (need complex conjugate mixing matrix) of energy Enu.
+@jit
 def getHVac2Enu( Mix,  dmVacVac, HVac2Enu):
+    '''
+    Calculate vacuum Hamiltonian in flavor basis for neutrino or 
+    antineutrino (need complex conjugate mixing matrix) of energy Enu.
+    '''
     dmVacDiag = np.zeros((3,3)) + np.zeros((3,3)) * 1j
     dmVacDiag[1,1] = dmVacVac[1,0]
     dmVacDiag[2,2] = dmVacVac[2,0]
     HVac2Enu = Mix.dot(dmVacDiag).dot(Mix.conj().T)
 
-# Calculate effective non-standard interaction Hamiltonian in flavor basis 
-def getHNSI(rho, NSIEps, antitype,  HNSI):
-     NSIRhoScale = 3. #// assume 3x electron density for "NSI"-quark (e.g., d) density
-     fact = NSIRhoScale * rho * tworttwoGf * antitype / 2.
-     HNSI = fact * NSIRhoScale
-     # only real NSI for now
-     HNSI = HNSI.real + np.zeros((3,3)) * 1j
+@jit
+def getHNSI(rho, NSIEps, antitype):
+    '''
+    Calculate effective non-standard interaction Hamiltonian in flavor basis 
+    '''
+    NSIRhoScale = 3. #// assume 3x electron density for "NSI"-quark (e.g., d) density
+    fact = NSIRhoScale * rho * tworttwoGf * antitype / 2.
+    HNSI = fact * NSIEps
+    # only real NSI for now
+    HNSI = HNSI.real + np.zeros((3,3)) * 1j
+    return HNSI
 
-# From the full matter Hamiltonian in flavor basis, transform to mass eigenstate
-#   basis unp.sing mixing matrix 
-def getHMatMassEigenstateBasis(Mix, HMat):
-    return Mix.conj().T.dot(HMat).dot(Mix)
-
-# Calculate full matter Hamiltonian in flavor basis 
+@jit
 def getHMat(rho, NSIEps, antitype):
-    # in the following, `a` is just the standard effective matter potential
-    # induced by charged-current weak interactions with electrons
-    # (modulo a factor of 2E)
+    '''
+    Calculate full matter Hamiltonian in flavor basis 
+
+    in the following, `a` is just the standard effective matter potential
+    induced by charged-current weak interactions with electrons
+    (modulo a factor of 2E)
+    '''
     a = rho * tworttwoGf * antitype / 2.
 
     HSI = np.zeros((3,3)) + np.zeros((3,3)) * 1j
-    HNSI = np.zeros((3,3)) + np.zeros((3,3)) * 1j
-
     HSI[elec,elec] = a
 
     # Obtain effective non-standard matter interaction Hamiltonian
-    getHNSI(rho, NSIEps, antitype, HNSI)
+    HNSI = getHNSI(rho, NSIEps, antitype)
 
     # This is where the full matter Hamiltonian is created
     return HSI + HNSI
 
-# add_complex_matrix adapted to vacuum and matter Hamiltonian
-def add_HVac_HMat(Enu, HVac2Enu,HMat):
-     return HVac2Enu / (2. * Enu) + HMat
-
-#**********************************************************************
-#  getM
-#  Compute the matter-mass vector M, dM = M_i-M_j and dMimj
-#**********************************************************************
-# Calculate mass eigenstates in matter of uniform density rho for
-#   neutrino or anti-neutrino (type already taken into account in Hamiltonian)
-#   of energy Enu. 
+@jit
 def getM(Enu, rho, dmVacVac, dmMatMat, dmMatVac, HMat):
+    '''
+    Compute the matter-mass vector M, dM = M_i-M_j and dMimj
+
+    Calculate mass eigenstates in matter of uniform density rho for
+    neutrino or anti-neutrino (type already taken into account in Hamiltonian)
+    of energy Enu. 
+    '''
 
     ReHEMuHMuTauHTauE = (HMat[elec,muon]*HMat[muon,tau]*HMat[tau,elec]).real
 
@@ -81,17 +84,13 @@ def getM(Enu, rho, dmVacVac, dmMatMat, dmMatVac, HMat):
 
     c2 = - np.trace(HMat.real)
 
-    #print c0, c1, c2
-
     c2V = (-1.0/(2.0*Enu))*(dmVacVac[1,0] + dmVacVac[2,0])
 
     p = c2*c2 - 3.0*c1
     pV = (1.0/(2.0*Enu*2.0*Enu))*(dmVacVac[1,0]*dmVacVac[1,0] +
                               dmVacVac[2,0]*dmVacVac[2,0] - 
                               dmVacVac[1,0]*dmVacVac[2,0])
-    if (p<0.0):
-      #printf("getM: p < 0 ! \n")
-      p = 0.0
+    p = max(0., p)
 
     q = -27.0*c0/2.0 - c2*c2*c2 + 9.0*c1*c2/2.0
     qV = (1.0/(2.0*Enu*2.0*Enu*2.0*Enu))*(
@@ -101,18 +100,16 @@ def getM(Enu, rho, dmVacVac, dmMatMat, dmMatVac, HMat):
 
     tmp = p*p*p - q*q
     tmpV = pV*pV*pV - qV*qV
-    if (tmp<0.0):
-        #printf("getM: p^3 - q^2 < 0 !\n")
-        tmp = 0.0
 
-    #print tmp
-    #print q
+    tmp = max(0., tmp)
+
     theta = np.array([np.arctan2(np.sqrt(tmp), q) / 3.] * 3)
     thetaV = np.array([np.arctan2(np.sqrt(tmpV), qV) / 3.] * 3)
-    theta[0] += (2.0/3.0)*np.pi
-    thetaV[0] += (2.0/3.0)*np.pi
-    theta[1] -= (2.0/3.0)*np.pi
-    thetaV[1] -= (2.0/3.0)*np.pi
+    a = (2./3.)*np.pi
+    theta[0] += a
+    thetaV[0] += a
+    theta[1] -= a
+    thetaV[1] -= a
 
     mMatU = 2.0*Enu*((2.0/3.0)*np.sqrt(p)*np.cos(theta) - c2/3.0 + dmVacVac[0,0])
     mMatV = 2.0*Enu*((2.0/3.0)*np.sqrt(pV)*np.cos(thetaV) - c2V/3.0 + dmVacVac[0,0])
@@ -132,14 +129,13 @@ def getM(Enu, rho, dmVacVac, dmMatMat, dmMatVac, HMat):
         for j in range(3):
               dmMatMat[i,j] = mMat[i] - mMat[j]
               dmMatVac[i,j] = mMat[i] - dmVacVac[j,0]
-    print dmMatMat
 
-
-#**********************************************************************
-# getA (take into account generic potential matrix (=Hamiltonian))
-# Calculate the transition amplitude matrix A (equation 10)
-#**********************************************************************
+@jit
 def getA(L, E, rho, Mix,  dmMatVac, dmMatMat, HMatMassEigenstateBasis, phase_offset):
+    '''
+    getA (take into account generic potential matrix (=Hamiltonian))
+    Calculate the transition amplitude matrix A (equation 10)
+    '''
 
     X = np.zeros((3,3)) + np.zeros((3,3)) * 1j
     product = np.zeros((3,3,3)) + np.zeros((3,3,3)) * 1j
@@ -151,17 +147,14 @@ def getA(L, E, rho, Mix,  dmMatVac, dmMatMat, HMatMassEigenstateBasis, phase_off
         arg = -LoEfac*dmMatVac[k,0]*L/E
         if ( k==2 ):
             arg += phase_offset 
-        c = np.cos(arg)
-        s = 1j * np.sin(arg)
-        X += c*product[:,:,k] - s*product[:,:,k]
+        X += (np.cos(arg) - 1j*np.sin(arg)) * product[:,:,k]
 
     # Compute the product with the mixing matrices 
     # is this correct?
     return Mix.dot(X).dot(Mix.conj().T)
 
-
+@jit
 def get_product(L, E, rho, dmMatVac, dmMatMat, HMatMassEigenstateBasis, product):
-
     twoEHmM = np.zeros((3,3,3)) + np.zeros((3,3,3)) * 1j
     twoEHmM[:,:,0] = 2. * E * HMatMassEigenstateBasis
     twoEHmM[:,:,1] = twoEHmM[:,:,0]
@@ -171,56 +164,51 @@ def get_product(L, E, rho, dmMatVac, dmMatMat, HMatMassEigenstateBasis, product)
         for j in range(3):
             twoEHmM[n,n,j] -= dmMatVac[j,n]
 
-  # Calculate the product in eq.(10) of twoEHmM for j!=k 
+    # Calculate the product in eq.(10) of twoEHmM for j!=k 
     product[:,:,0] = (twoEHmM[:,:,1].dot(twoEHmM[:,:,2])) / (dmMatMat[0,1] * dmMatMat[0,2])
     product[:,:,1] = (twoEHmM[:,:,2].dot(twoEHmM[:,:,0])) / (dmMatMat[1,2] * dmMatMat[1,0])
     product[:,:,2] = (twoEHmM[:,:,0].dot(twoEHmM[:,:,1])) / (dmMatMat[2,0] * dmMatMat[2,1])
 
-
-
+@jit
 def convert_from_mass_eigenstate(state, pure, mixNuType):
     mass = np.zeros((3)) + np.zeros((3)) * 1j
     lstate  = state - 1
 
     for i in range(3): 
         mass[i] = 1. if lstate == i else 0.
-    #note: mixNuType is already taking into account whether we're considering
+    # note: mixNuType is already taking into account whether we're considering
     # nu or anti-nu
     pure = mixNuType.dot(mass)
 
-
-# Calculate neutrino flavour transition amplitude matrix for neutrino (nutype > 0)
-#   or antineutrino (nutype < 0) with energy Enu travernp.sing layer of matter of
-#   uniform density rho with thickness Len.
-
+@jit
 def get_transition_matrix(nutype, Enu, rho, Len,
                            phase_offset,
                            mixNuType,  nsi_eps,
                            HVac2Enu,  dm):
+    '''
+    Calculate neutrino flavour transition amplitude matrix for neutrino (nutype > 0)
+    or antineutrino (nutype < 0) with energy Enu travernp.sing layer of matter of
+    uniform density rho with thickness Len.
+    '''
     dmMatVac = np.zeros((3,3))
     dmMatMat = np.zeros((3,3))
-    #HFull = np.zeros((3,3)) + np.zeros((3,3)) * 1j
-    #HMat = np.zeros((3,3)) + np.zeros((3,3)) * 1j
-    #HMatMassEigenstateBasis = np.zeros((3,3)) + np.zeros((3,3)) * 1j
 
     # Compute the matter potential including possible non-standard interactions
-    #   in the flavor basis 
+    # in the flavor basis 
     HMat = getHMat(rho, nsi_eps, nutype)
 
     # Get the full Hamiltonian by adding together matter and vacuum parts 
-    HFull = add_HVac_HMat(Enu, HVac2Enu, HMat)
+    HFull = HVac2Enu / (2. * Enu) + HMat
 
     # Calculate modified mass eigenvalues in matter from the full Hamiltonian and
-    #   the vacuum mass splittings 
+    # the vacuum mass splittings 
     getM(Enu, rho, dm, dmMatMat, dmMatVac, HFull)
 
     # Now we transform the matter (TODO: matter? full?) Hamiltonian back into the
-    #   mass eigenstate basis so we don't need to compute products of the effective
-    #   mixing matrix elements explicitly 
-    print 'mixNuType: ', mixNuType
-    HMatMassEigenstateBasis = getHMatMassEigenstateBasis(mixNuType, HMat)
-    print 'HMatMassEigenstateBasis: ',HMatMassEigenstateBasis
+    # mass eigenstate basis so we don't need to compute products of the effective
+    # mixing matrix elements explicitly 
+    HMatMassEigenstateBasis = mixNuType.conj().T.dot(HMat).dot(mixNuType)
 
     # We can now proceed to calculating the transition amplitude from the Hamiltonian
-    #   in the mass basis and the effective mass splittings 
+    # in the mass basis and the effective mass splittings 
     return getA(Len, Enu, rho, mixNuType, dmMatVac, dmMatMat, HMatMassEigenstateBasis, phase_offset)
--- pisa/stages/osc/test_numba_osc.py
+++ pisa/stages/osc/test_numba_osc.py
@@ -1,78 +1,67 @@
+from __future__ import print_function
 import numpy as np
 from  propy.osc import *
 from pisa.stages.osc.osc_params import *
+from numba import jit, vectorize, guvectorize, float64, complex64, int32, float32, complex128
 
-
-def propagateArray(d_prob_e,
-                   d_prob_mu,
-                   d_dm,
-                   d_mix,
-                   d_nsi_eps,
-                   n_evts,
+@guvectorize([(float64[:,:], complex128[:,:], float64[:,:], int32[:], int32[:], int32, float64[:], int32[:], float64[:], float64[:], float64[:,:])], '(a,b),(c,d),(e,f),(),(),(),(),(),(g),(h)->(a,b)')
+def propagateArray(dm,
+                   mix,
+                   nsi_eps,
                    kNuBar,
                    kFlav,
                    maxLayers,
-                   true_e_scale,
-                   d_energy,
-                   d_numberOfLayers,
-                   d_densityInLayer,
-                   d_distanceInLayer):
+                   energy,
+                   numberOfLayers,
+                   densityInLayer,
+                   distanceInLayer,
+                   Probability):
 
     kUseMassEstates = False
+    #print('knubar = ',kNuBar)
+    #print('mix = ',mix)
 
-    #/*
     #TODO: * ensure convention below is respected in MC reweighting
     #          (kNuBar > 0 for nu, < 0 for anti-nu)
     #        * kNuBar is passed in, so could already pass in the correct form
     #          of mixing matrix, i.e., possibly conjugated
-    #*/
-    if (kNuBar > 0):
+    if (kNuBar[0] > 0):
         # in this case the mixing matrix is left untouched
-        #copy_complex_matrix(d_mix, mixNuType)
-        mixNuType = d_mix
+        mixNuType = mix
     
     else:
         # here we need to complex conjugate all entries
         # (note that this only changes calculations with non-zero deltacp)
-        #conjugate_complex_matrix(d_mix, mixNuType)
-        mixNuType = d_mix.conj().T
+        #print('mix: ',mix)
+        mixNuType = mix.conj().T
 
     HVac2Enu = np.zeros((3,3)) + np.zeros((3,3)) * 1j
 
-    getHVac2Enu(mixNuType, d_dm, HVac2Enu)
-
-    #define those
-    #TransitionMatrix = np.zeros((3,3)) + np.zeros((3,3)) * 1j
-    #TransitionProduct = np.zeros((3,3)) + np.zeros((3,3)) * 1j
-    #TransitionTemp = np.zeros((3,3)) + np.zeros((3,3)) * 1j
+    getHVac2Enu(mixNuType, dm, HVac2Enu)
 
     RawInputPsi = np.zeros((3)) + np.zeros((3)) * 1j
-    Probability = np.zeros((3,3))
 
-    layers = d_numberOfLayers
-    energy = d_energy * true_e_scale
+    layers = numberOfLayers[0]
+    #print('layers = ',layers)
     for i in range(layers):
-        density = d_densityInLayer[i]
-        distance = d_distanceInLayer[i]
+        density = densityInLayer[i]
+        distance = distanceInLayer[i]
         TransitionMatrix = get_transition_matrix(kNuBar,
                                energy,
                                density,
                                distance,
                                0.0,
-                               mixNuType, d_nsi_eps, HVac2Enu,
-                               d_dm)
-        #print TransitionMatrix
+                               mixNuType,
+                               nsi_eps,
+                               HVac2Enu,
+                               dm)
         if (i==0):
-            #copy_complex_matrix(TransitionMatrix, TransitionProduct)
             TransitionProduct = TransitionMatrix
         else:
             TransitionProduct = TransitionMatrix * TransitionProduct 
-            #clear_complex_matrix( TransitionTemp )
-            #multiply_complex_matrix( TransitionMatrix, TransitionProduct, TransitionTemp )
-            #copy_complex_matrix( TransitionTemp, TransitionProduct )
         
-      # loop on neutrino types, and compute probability for neutrino i:
-      # We actually don't care about nutau -> anything since the flux there is zero!
+    # loop on neutrino types, and compute probability for neutrino i:
+    # We actually don't care about nutau -> anything since the flux there is zero!
     for i in range(2):
         for j in range(3):
             RawInputPsi[j] = 0.0 + 0.0j
@@ -88,58 +77,55 @@ def propagateArray(d_prob_e,
         Probability[i][1] += abs(OutputPsi[1])
         Probability[i][2] += abs(OutputPsi[2])
 
-    d_prob_e = Probability[0][kFlav]
-    d_prob_mu = Probability[1][kFlav]
-    print 'prob_e: ', d_prob_e
-    print 'prob_mu: ', d_prob_mu
+    #prob_e = Probability[0][kFlav]
+    #prob_mu = Probability[1][kFlav]
+    #print 'prob_e: ', prob_e
+    #print 'prob_mu: ', prob_mu
 
-
-d_prob_e = np.zeros(10)
-d_prob_mu = np.ones(10)
 
 
 OP = OscParams(0.002, 0.02, 0.3, 0.1, 0.5, 0)
 
-d_mix = OP.mix_matrix[:,:,0] + OP.mix_matrix[:,:,1] * 1j
-d_dm = OP.dm_matrix
+mix = OP.mix_matrix[:,:,0] + OP.mix_matrix[:,:,1] * 1j
+dm = OP.dm_matrix
 
-d_nsi_eps = np.zeros((3,3))
+nsi_eps = np.zeros((3,3))
 
-n_evts = 10
+nevts = 10
 
-kNuBar = 1
+# input arrays
+# nu /nu-bar
+kNuBar = np.ones(nevts, dtype=np.int32)
+# flavours
+kFlav = np.ones(nevts, dtype=np.int32)
 
-kFlav = 1
+energy = np.linspace(1,10,nevts)
 
-maxLayers = 1
+# Layers
+maxLayers = np.int32(1)
+numberOfLayers = np.ones((nevts), dtype=np.int32)
+densityInLayer = np.ones((nevts,1))
+distanceInLayer = np.ones((nevts,1))
 
-true_e_scale = 1.
-
-d_energy = np.linspace(1,10,10)
-
-d_numberOfLayers = np.ones((10), dtype=np.int)
-
-# needs to be 2d
-d_densityInLayer = np.ones((10,1))
-d_distanceInLayer = np.ones((10,1))
+# empty arrays to be filled
+#prob_e = np.zeros(nevts)
+#prob_mu = np.ones(nevts)
+Probability = np.zeros((nevts,3,3))
 
 i=0
 
-propagateArray(d_prob_e[i],
-                   d_prob_mu[i],
-                   d_dm,
-                   d_mix,
-                   d_nsi_eps,
-                   n_evts,
-                   kNuBar,
-                   kFlav,
-                   maxLayers,
-                   true_e_scale,
-                   d_energy[i],
-                   d_numberOfLayers[i],
-                   d_densityInLayer[i],
-                   d_distanceInLayer[i])
+propagateArray(
+               dm,
+               mix,
+               nsi_eps,
+               kNuBar,
+               kFlav,
+               maxLayers,
+               energy,
+               numberOfLayers,
+               densityInLayer,
+               distanceInLayer,
+               Probability)
 
-#print d_prob_e
-#print d_prob_mu
+print(Probability)
 
