3cbfee8847b43748f90dad3a8f419636884d9f2f
Philipp Eller <peller@phys.ethz.ch>
2017-10-24 13:33:39 -0400

    cleanup part IV

--- pisa/stages/osc/numba_osc.py
+++ pisa/stages/osc/numba_osc.py
@@ -1,125 +1,11 @@
 from __future__ import print_function
+
 import numpy as np
-import time
-import inspect
 from numba import jit, vectorize, guvectorize, float64, complex64, int32, float32, complex128
 import math, cmath
 
-#target='cuda'
-#target='parallel'
-target='cpu'
+from numba_tools import *
 
-if target == 'cuda':
-    from numba import cuda
-    ctype = complex128
-    ftype = float64
-else:
-    ctype = np.complex128
-    ftype = np.float64
-    cuda = lambda: None
-    cuda.jit = lambda x: x
-
-def myjit(f):
-    '''
-    f : function
-
-    Decorator to assign the right jit for different targets
-    In case of non-cuda targets, all instances of `cuda.local.array`
-    are replaced by `np.empty`. This is a dirty fix, hopefully in the
-    near future numba will support numpy array allocation and this will
-    not be necessary anymore
-    '''
-    if target == 'cuda':
-        return cuda.jit(f, device=True)
-    else:
-        source = inspect.getsource(f).splitlines()
-        assert '@myjit' in source[0]
-        source = '\n'.join(source[1:]) + '\n'
-        source = source.replace('cuda.local.array', 'np.empty')
-        exec(source)
-        fun = eval(f.__name__)
-        newfun = jit(fun, nopython=True)
-        # needs to be exported to globals
-        globals()[f.__name__] = newfun
-        return newfun
-
-@myjit
-def conjugate_transpose(A, B):
-    '''
-    A : 2d array
-    B : 2d array
-
-    B is the conjugate transpose of A
-    '''
-    for i in range(A.shape[0]):
-        for j in range(A.shape[1]):
-            B[i,j] = A[j,i].conjugate()
-
-@myjit
-def matrix_dot_matrix(A, B, C):
-    '''
-    dot-product of two 2d arrays
-    C = A * B
-    '''
-    for j in range(B.shape[1]):
-        for i in range(A.shape[0]):
-            C[i,j] = 0.
-            for n in range(C.shape[0]):
-                C[i,j] += A[i,n] * B[n,j]
-
-def test_matrix_dot_matrix():
-    A = np.linspace(1., 8., 9).reshape(3,3)
-    B = np.linspace(1., 8., 9).reshape(3,3)
-    C = np.zeros((3,3))
-    matrix_dot_matrix(A, B, C)
-    assert np.array_equal(C, np.dot(A, B))
-
-@myjit
-def matrix_dot_vector(A, v, w):
-    '''
-    dot-product of a 2d array and a vector
-    w = A * v
-    '''
-    for i in range(A.shape[0]):
-        w[i] = 0.
-        for j in range(A.shape[1]):
-            w[i] += A[i,j] * v[j]
-
-def test_matrix_dot_vector():
-    A = np.linspace(1., 8., 9).reshape(3,3)
-    v = np.linspace(1., 3., 3)
-    w = np.zeros((3))
-    matrix_dot_vector(A, v, w)
-    assert np.array_equal(w, np.dot(A, v))
-
-@myjit
-def clear_matrix(A):
-    '''
-    clear out 2d array
-    '''
-    for i in range(A.shape[0]):
-        for j in range(A.shape[1]):
-            A[i,j] = 0.
-
-def test_clear_matrix():
-    A = np.ones((3,3))
-    clear_matrix(A)
-    assert np.array_equal(A, np.zeros((3,3)))
-
-@myjit
-def copy_matrix(A, B):
-    '''
-    copy elemnts of 2d array A to array B
-    '''
-    for i in range(A.shape[0]):
-        for j in range(A.shape[1]):
-            B[i,j] = A[i,j]
-
-def test_copy_matrix():
-    A = np.ones((3,3))
-    B = np.zeros((3,3))
-    copy_matrix(A, B)
-    assert np.array_equal(A, B)
 
 @myjit
 def get_H_vac(mix_nubar, delta_M_vac_vac, H_vac):
@@ -742,23 +628,12 @@ def test_propagate_array_kernel():
 
 if __name__=='__main__':
     
-    test_matrix_dot_matrix()
-    test_matrix_dot_vector()
-    test_clear_matrix()
-    test_copy_matrix()
+    assert target == 'cpu', "Cannot test functions on GPU, set target='cpu'"
     test_get_H_vac()
-    print('test successfull')
     test_get_H_mat()
-    print('test successfull')
     test_get_delta_Ms()
-    print('test successfull')
     test_get_product()
-    print('test successfull')
     test_get_transition_matrix()
-    print('test successfull')
     test_convert_from_mass_eigenstate()
-    print('test successfull')
     test_get_transition_matrix()
-    print('test successfull')
     test_propagate_array_kernel()
-    print('test porpagate array kernel successfull')
