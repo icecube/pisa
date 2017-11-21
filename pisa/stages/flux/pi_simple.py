8a646ed94ea23e13d88ec37f8d3f97e844b8e580
Philipp Eller <peller@phys.ethz.ch>
2017-11-21 18:32:10 -0500

    avoid division by zero for zero flux

--- pisa/stages/flux/pi_simple.py
+++ pisa/stages/flux/pi_simple.py
@@ -130,6 +130,11 @@ def apply_ratio_scale(ratio_scale, sum_constant, in1, in2, out):
     out : array
     
     '''
+    if in1 == 0. and in2 == 0.:
+        out[0] == 0.
+        out[1] == 0.
+        return
+
     if sum_constant:
         orig_ratio = in1 / in2
         orig_sum = in1 + in2
@@ -163,50 +168,55 @@ def LogLogParam(true_energy,  y1,  y2,  x1,  x2, use_cutoff, cutoff_value):
         modification *= math.exp(-1. * true_energy / cutoff_value)
     return modification
 
-# These parameters are obtained from fits to the paper of Barr
-# E dependent ratios, max differences per flavor (Fig.7)
-e1max_mu = 3.
-e2max_mu = 43
-e1max_e  = 2.5
-e2max_e  = 10
-e1max_mu_e = 0.62
-e2max_mu_e = 11.45
-# Evaluated at
-x1e = 0.5
-x2e = 3.
-
-# Zenith dependent amplitude, max differences per flavor (Fig. 9)
-z1max_mu = 0.6
-z2max_mu = 5.
-z1max_e  = 0.3
-z2max_e  = 5.
-nue_cutoff  = 650.
-numu_cutoff = 1000.
-# Evaluated at
-x1z = 0.5
-x2z = 2.
-
 @myjit
 def norm_fcn(x, A, sigma):
     # oscfit function
     return A / math.sqrt(2 * math.pi * math.pow(sigma,2)) * math.exp(-math.pow(x, 2) / (2 * math.pow(sigma, 2)))
 
 @myjit
-def ModNuMuFlux(true_energy,  true_coszen,  e1,  e2,  z1,  z2):
-    # oscfit function
-    A_ave = LogLogParam(true_energy, e1max_mu*e1, e2max_mu*e2, x1e, x2e, False, 0)
-    A_shape = 2.5*LogLogParam(true_energy, z1max_mu*z1, z2max_mu*z2, x1z, x2z, True, numu_cutoff)
-    return A_ave - (norm_fcn(true_coszen, A_shape, 0.36) - 0.6 * A_shape)
+def ModFlux(flav, true_energy,  true_coszen,  e1mu,  e2mu,  z1mu,  z2mu,  e1e,  e2e,  z1e,  z2e):
+    # These parameters are obtained from fits to the paper of Barr
+    # E dependent ratios, max differences per flavor (Fig.7)
+    e1max_mu = 3.
+    e2max_mu = 43
+    e1max_e  = 2.5
+    e2max_e  = 10
+    # Evaluated at
+    x1e = 0.5
+    x2e = 3.
 
-@myjit
-def ModNuEFlux(true_energy,  true_coszen,  e1mu,  e2mu,  z1mu,  z2mu,  e1e,  e2e,  z1e,  z2e):
+    # Zenith dependent amplitude, max differences per flavor (Fig. 9)
+    z1max_mu = 0.6
+    z2max_mu = 5.
+    z1max_e  = 0.3
+    z2max_e  = 5.
+    nue_cutoff  = 650.
+    numu_cutoff = 1000.
+    # Evaluated at
+    x1z = 0.5
+    x2z = 2.
     # oscfit function
-    A_ave = LogLogParam(true_energy, e1max_mu * e1mu + e1max_e * e1e, e2max_mu * e2mu + e2max_e * e2e, x1e, x2e, False, 0)
-    A_shape = 1. * LogLogParam(true_energy, z1max_mu * z1mu + z1max_e * z1e, z2max_mu * z2mu + z2max_e * z2e, x1z, x2z, True, nue_cutoff)
-    return A_ave - (1.5*norm_fcn(true_coszen, A_shape, 0.36) - 0.7 * A_shape)
+    if flav == 1:
+        A_ave = LogLogParam(true_energy, e1max_mu*e1mu, e2max_mu*e2mu, x1e, x2e, False, 0)
+        A_shape = 2.5*LogLogParam(true_energy, z1max_mu*z1mu, z2max_mu*z2mu, x1z, x2z, True, numu_cutoff)
+        return A_ave - (norm_fcn(true_coszen, A_shape, 0.36) - 0.6 * A_shape)
+    if flav == 0:
+        A_ave = LogLogParam(true_energy, e1max_mu * e1mu + e1max_e * e1e, e2max_mu * e2mu + e2max_e * e2e, x1e, x2e, False, 0)
+        A_shape = 1. * LogLogParam(true_energy, z1max_mu * z1mu + z1max_e * z1e, z2max_mu * z2mu + z2max_e * z2e, x1z, x2z, True, nue_cutoff)
+        return A_ave - (1.5*norm_fcn(true_coszen, A_shape, 0.36) - 0.7 * A_shape)
 
 @myjit
 def modRatioUpHor(flav, true_energy, true_coszen, uphor):
+    # Zenith dependent amplitude, max differences per flavor (Fig. 9)
+    z1max_mu = 0.6
+    z2max_mu = 5.
+    z1max_e  = 0.3
+    z2max_e  = 5.
+    nue_cutoff  = 650.
+    numu_cutoff = 1000.
+    # Evaluated at
+    x1z = 0.5
+    x2z = 2.
     # oscfit function
     if flav == 0:
         A_shape = 1. * abs(uphor) * LogLogParam(true_energy, (z1max_e + z1max_mu), (z2max_e + z2max_mu), x1z, x2z, True, nue_cutoff)
@@ -219,10 +229,7 @@ def modRatioNuBar(nubar, flav, true_energy, true_coszen, nu_nubar, nubar_sys):
     # oscfit function
     #not sure what nu_nubar is, only found this line in the documentation:
     # +1 applies the change to neutrinos, 0 to antineutrinos. Anything in between is shared
-    if flav == 0:
-        modfactor = nubar_sys * ModNuEFlux(true_energy, true_coszen, 1., 1., 1., 1., 1., 1., 1., 1.)
-    if flav == 1:
-        modfactor = nubar_sys * ModNuMuFlux(true_energy, true_coszen, 1., 1., 1., 1.)
+    modfactor = nubar_sys * ModFlux(flav, true_energy, true_coszen, 1., 1., 1., 1., 1., 1., 1., 1.)
     if nubar < 0:
         return max(0., 1. / (1 + 0.5 * modfactor))
     if nubar > 0:
