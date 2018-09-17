37647facf86dcb31896575aa8d661068ff605bdb
J.L. Lanfranchi <jll1062@phys.psu.edu>
2018-09-17 19:54:09 -0400

    add author, copyright

--- pisa/scripts/fit_discrete_sys_nd.py
+++ pisa/scripts/fit_discrete_sys_nd.py
@@ -19,16 +19,15 @@ found in fridge/analysis/common/scripts/plotting/plot_hyperplane_fits.py
 
 from __future__ import absolute_import, division, print_function
 
+from argparse import ArgumentParser
+from collections import Mapping, Sequence, OrderedDict
 import copy
 from os.path import basename, join, splitext
 
-from argparse import ArgumentParser
-from collections import Mapping, Sequence, OrderedDict
-from uncertainties import unumpy as unp
-from uncertainties import ufloat
-
 import numpy as np
 from scipy.optimize import curve_fit
+from uncertainties import unumpy as unp
+from uncertainties import ufloat
 
 from pisa.core.distribution_maker import DistributionMaker
 from pisa.core.map import Map, MapSet
@@ -48,6 +47,22 @@ __all__ = [
     "save_hyperplane_fits",
     "main",
 ]
+
+__author__ = "P. Eller, T. Stuttard, T. Ehrhardt"
+
+__license__ = """Copyright (c) 2014-2018, The IceCube Collaboration
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License."""
 
 
 def parse_args():
