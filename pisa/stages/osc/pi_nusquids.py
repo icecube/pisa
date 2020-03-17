icecube/pisa

commit d5b40edbd14bf306787d75c8b116f2f67c655513

Refactor vectorizer (issue #573), misc cleanups (#574)

pisa/stages/osc/pi_nusquids.py
@@ -24,12 +24,9 @@ from pisa.utils.numba_tools import WHERE
 from pisa.stages.osc.pi_prob3 import apply_probs
 
 from pisa.utils.resources import find_resource
-from pisa.utils import vectorizer
 from pisa.stages.osc.nusquids.nusquids_osc import NSQ_CONST, validate_calc_grid, compute_binning_constants, init_nusquids_prop, evolve_states, osc_probs, earth_model
 from pisa import ureg
 
-
-from scipy.interpolate import RectBivariateSpline
 
 __all__ = ['pi_nusquids']
 

pisa/core/bin_indexing.py
@@ -1,32 +1,23 @@
 """
-Functions to retrieve the bin location of each elements
-of an array, inside a Container, based on its specified
-output binning.
+Functions to retrieve the bin index for a 1- to 3-dimensional sample.
 
-functions were adapted from translation.py
+Functions were adapted from translation.py
 
 
-NOTE:
-----
+Notes
+-----
+The binning convention in PISA (from numpy.histogramdd) is that the lower edge
+is inclusive and upper edge is exclusive for a given bin, except for the
+upper-most bin whose upper edge is also inclusive. Visually, for 1D:
 
-binning convention in pisa is that both lower and upper
-bounds of an inner bin edges is included in the lower bin
+    [ bin 0 ) [ bin 1 ) ... [ bin num_bins - 1 ]
 
-first bin is bin 0
+First bin is index = 0 and last bin is index = (num_bins - 1)
 
-last bin is bin (NBins -1)
-
-bounds falling on the lowest bin edge is included in bin 0
-
-bounds falling in the highest bin edge is included in bin Nbins
-
-values falling below the lowest edge have index -1
-
-values falling above the highest edge have index Nbins
-
-if a value falls on an inner edge, it is included in the lowest
-bin bound by that edge
-
+* Values below the lowermost-edge of any dimension's binning return index = -1
+* NaN values return index = -1
+* Otherwise, values above the uppermost-edge of any dimension's binning return
+  index = num_bins
 
 """
 
@@ -37,124 +28,37 @@ from numba import guvectorize, SmartArray
 
 from pisa import FTYPE, TARGET
 from pisa.core.binning import OneDimBinning, MultiDimBinning
+from pisa.core.translation import find_index
 from pisa.utils.log import logging, set_verbosity
 from pisa.utils.numba_tools import WHERE
 
-from translation import find_index
 
 __all__ = ["lookup_indices", "test_lookup_indices"]
 
 
-# ---------- Lookup methods ---------------
+FX = "f4" if FTYPE == np.float32 else "f8"
 
 
-def lookup_indices(sample, binning):
-    """The inverse of histograming
-
-    Paramters
-    ---------
-    sample : list of SmartArrays
-
-    binning : pisa.core.binning.OneDimBinning or pisa.core.binning.MultiDimBinning
-
-    Returns: for each event the index of the histogram in which it falls into
-
-    Notes
-    -----
-    this method works for 1d, 2d and 3d histogram only
-
-    """
-    if isinstance(binning, OneDimBinning):
-        binning = MultiDimBinning(binning)
-    if not isinstance(binning, MultiDimBinning):
-        raise TypeError(
-            "binning must be OneDimBinning or MultiDimBinning; got {}".format(
-                type(binning)
-            )
-        )
-    assert 1 <= binning.num_dims <= 3, "can only do 1d, 2d and 3d at the moment"
-
-    bin_edges = [edges.magnitude for edges in binning.bin_edges]
-
-    array = SmartArray(np.zeros_like(sample[0], dtype=np.int64))
-
-    if binning.num_dims == 1:
-
-        assert (
-            len(sample) == 1
-        ), "ERROR: binning provided has 1 dimension, but sample provided has not"
-
-        lookup_index_vectorized_1d(
-            sample[0].get(WHERE), bin_edges[0], out=array.get(WHERE)
-        )
-
-    elif binning.num_dims == 2:
-
-        assert (
-            len(sample) == 2
-        ), "ERROR: binning provided has 2 dimensions, but sample provided has not."
-
-        lookup_index_vectorized_2d(
-            sample[0].get(WHERE),
-            sample[1].get(WHERE),
-            bin_edges[0],
-            bin_edges[1],
-            out=array.get(WHERE),
-        )
-
-    elif binning.num_dims == 3:
-
-        assert (
-            len(sample) == 3
-        ), "ERROR: binning provided has 3 dimensions, but sample provided has not."
-
-        lookup_index_vectorized_3d(
-            sample[0].get(WHERE),
-            sample[1].get(WHERE),
-            sample[2].get(WHERE),
-            bin_edges[0],
-            bin_edges[1],
-            bin_edges[2],
-            out=array.get(WHERE),
-        )
-
-    else:
-        raise NotImplementedError()
-
-    array.mark_changed(WHERE)
-    return array
+@guvectorize(
+    [f"({FX}[:], {FX}[:], i8[:])"],
+    "(), (j) -> ()",
+    target=TARGET,
+)
+def lookup_indices_vectorized_1d(sample_x, bin_edges_x, out):
+    """Lookup bin indices for sample_x values, where binning is defined by
+    `bin_edges_x`."""
+    out[0] = find_index(val=sample_x[0], bin_edges=bin_edges_x)
 
 
-# -----------------------------------------------------------------------
-# Numba vectorized functions
-
-if FTYPE == np.float32:
-    _SIGNATURE = ["(f4[:], f4[:], i8[:])"]
-else:
-    _SIGNATURE = ["(f8[:], f8[:], i8[:])"]
-
-
-@guvectorize(_SIGNATURE, "(), (j) -> ()", target=TARGET)
-def lookup_index_vectorized_1d(sample_x, bin_edges_x, out):
-    sample_x_ = sample_x[0]
-    idx = find_index(sample_x_, bin_edges_x)
-    out[0] = idx
-
-
-if FTYPE == np.float32:
-    _SIGNATURE = ["(f4[:], f4[:], f4[:], f4[:], i8[:])"]
-else:
-    _SIGNATURE = ["(f8[:], f8[:], f8[:], f8[:], i8[:])"]
-
-
-@guvectorize(_SIGNATURE, "(), (), (j), (k) -> ()", target=TARGET)
-def lookup_index_vectorized_2d(sample_x, sample_y, bin_edges_x, bin_edges_y, out):
+@guvectorize(
+    [f"({FX}[:], {FX}[:], {FX}[:], {FX}[:], i8[:])"],
+    "(), (), (a), (b) -> ()",
+    target=TARGET,
+)
+def lookup_indices_vectorized_2d(sample_x, sample_y, bin_edges_x, bin_edges_y, out):
     """Same as above, except we get back the index"""
-    sample_x_ = sample_x[0]
-    sample_y_ = sample_y[0]
-
-    idx_x = find_index(sample_x_, bin_edges_x)
-    idx_y = find_index(sample_y_, bin_edges_y)
+    idx_x = find_index(val=sample_x[0], bin_edges=bin_edges_x)
+    idx_y = find_index(val=sample_y[0], bin_edges=bin_edges_y)
 
     n_x_bins = len(bin_edges_x) - 1
     n_y_bins = len(bin_edges_y) - 1
@@ -167,28 +71,21 @@ def lookup_index_vectorized_2d(sample_x, sample_y, bin_edges_x, bin_edges_y, out
         # any dim overflowed
         out[0] = n_bins
     else:
-        idx = idx_x * n_y_bins + idx_y
-        out[0] = idx
+        out[0] = idx_x * n_y_bins + idx_y
 
 
-if FTYPE == np.float32:
-    _SIGNATURE = ["(f4[:], f4[:], f4[:], f4[:], f4[:], f4[:], i8[:])"]
-else:
-    _SIGNATURE = ["(f8[:], f8[:], f8[:], f8[:], f8[:], f8[:], i8[:])"]
-
-
-@guvectorize(_SIGNATURE, "(), (), (), (j), (k), (l) -> ()", target=TARGET)
-def lookup_index_vectorized_3d(
+@guvectorize(
+    [f"({FX}[:], {FX}[:], {FX}[:], {FX}[:], {FX}[:], {FX}[:], i8[:])"],
+    "(), (), (), (a), (b), (c) -> ()",
+    target=TARGET,
+)
+def lookup_indices_vectorized_3d(
     sample_x, sample_y, sample_z, bin_edges_x, bin_edges_y, bin_edges_z, out
 ):
     """Vectorized gufunc to perform the lookup"""
-    sample_x_ = sample_x[0]
-    sample_y_ = sample_y[0]
-    sample_z_ = sample_z[0]
-
-    idx_x = find_index(sample_x_, bin_edges_x)
-    idx_y = find_index(sample_y_, bin_edges_y)
-    idx_z = find_index(sample_z_, bin_edges_z)
+    idx_x = find_index(val=sample_x[0], bin_edges=bin_edges_x)
+    idx_y = find_index(val=sample_y[0], bin_edges=bin_edges_y)
+    idx_z = find_index(val=sample_z[0], bin_edges=bin_edges_z)
 
     n_x_bins = len(bin_edges_x) - 1
     n_y_bins = len(bin_edges_y) - 1
@@ -202,8 +99,74 @@ def lookup_index_vectorized_3d(
         # any dim overflowed
         out[0] = n_bins
     else:
-        idx = (idx_x * n_y_bins + idx_y) * n_z_bins + idx_z
-        out[0] = idx
+        out[0] = (idx_x * n_y_bins + idx_y) * n_z_bins + idx_z
+
+
+def lookup_indices(sample, binning):
+    """Lookup (flattened) bin index for sample points.
+
+    Parameters
+    ----------
+    sample : length-M_dimensions sequence of length-N_events SmartArrays
+        All smart arrays must have the same lengths; corresponding elements of
+        the arrays are the coordinates of an event in the dimensions each array
+        represents.
+
+    binning : pisa.core.binning.MultiDimBinning or convertible thereto
+        `binning` is passed to instantiate ``MultiDimBinning``, so e.g., a
+        pisa.core.binning.OneDimBinning is valid to pass as `binning`
+
+    Returns
+    -------
+    indices : length-N_events SmartArray
+        One for each event the index of the histogram in which it falls into
+
+    Notes
+    -----
+    this method works for 1d, 2d and 3d histogram only
+
+    """
+    # Convert non-MultiDimBinning objects into MultiDimBinning if possible;
+    # if this fails, an error will result, as it should
+    binning = MultiDimBinning(binning)
+
+    if len(sample) != binning.num_dims:
+        raise ValueError(
+            f"`binning` has {binning.num_dims} dimension(s), but `sample`"
+            f"contains {len(sample)} arrays (so represents {len(sample)}"
+            f" dimensions)"
+        )
+
+    lookup_funcs = {
+        1: lookup_indices_vectorized_1d,
+        2: lookup_indices_vectorized_2d,
+        3: lookup_indices_vectorized_3d,
+    }
+
+    if binning.num_dims not in lookup_funcs:
+        raise NotImplementedError(
+            "binning must have num_dims in {}; got {}".format(
+                sorted(lookup_funcs.keys()), binning.num_dims
+            )
+        )
+
+    lookup_func = lookup_funcs[binning.num_dims]
+
+    lookup_func_args = (
+        [a.get(WHERE) for a in sample]
+        + [SmartArray(dim.edge_magnitudes).get(WHERE) for dim in binning]
+    )
+    logging.trace("lookup_func_args = {}".format(lookup_func_args))
+
+    # Create an array to store the results
+    indices = SmartArray(np.empty_like(sample[0], dtype=np.int64))
+
+    # Perform the lookup
+    lookup_func(*lookup_func_args, out=indices.get(WHERE))
+
+    indices.mark_changed(WHERE)
+
+    return indices
 
 
 def test_lookup_indices():
@@ -246,7 +209,9 @@ def test_lookup_indices():
     indices = lookup_indices([x], binning_1d)
     logging.trace("indices of each array element: {}".format(indices.get()))
     logging.trace("*********************************")
-    assert np.array_equal(indices.get(), np.array([-1, 0, 1, 6, 6, 7, 6]))
+    test = indices.get()
+    ref = np.array([-1, 0, 1, 6, 6, 7, 6])
+    assert np.array_equal(test, ref), "test={} != ref={}".format(test, ref)
 
     # 2D case:
     #
@@ -255,14 +220,14 @@ def test_lookup_indices():
     #
     logging.trace("TEST 2D:")
     logging.trace("Total number of bins: {}".format(7 * 4))
-    logging.trace(
-        "array in 2D: {}".format([(i, j) for i, j in zip(x.get(), y.get())])
-    )
+    logging.trace("array in 2D: {}".format(list(zip(x.get(), y.get()))))
     logging.trace("Binning: {}".format(binning_2d.bin_edges))
     indices = lookup_indices([x, y], binning_2d)
     logging.trace("indices of each array element: {}".format(indices.get()))
     logging.trace("*********************************")
-    assert np.array_equal(indices.get(), np.array([-1, 0, 5, 25, 27, 28, 26]))
+    test = indices.get()
+    ref = np.array([-1, 0, 5, 25, 27, 28, 26])
+    assert np.array_equal(test, ref), "test={} != ref={}".format(test, ref)
 
     # 3D case:
     #
@@ -271,16 +236,14 @@ def test_lookup_indices():
     #
     logging.trace("TEST 3D:")
     logging.trace("Total number of bins: {}".format(7 * 4 * 2))
-    logging.trace(
-        "array in 3D: {}".format(
-            [(i, j, k) for i, j, k in zip(x.get(), y.get(), z.get())]
-        )
-    )
+    logging.trace("array in 3D: {}".format(list(zip(x.get(), y.get(), z.get()))))
     logging.trace("Binning: {}".format(binning_3d.bin_edges))
     indices = lookup_indices([x, y, z], binning_3d)
     logging.trace("indices of each array element: {}".format(indices.get()))
     logging.trace("*********************************")
-    assert np.array_equal(indices.get(), np.array([-1, 0, 11, 51, 54, 56, 52]))
+    test = indices.get()
+    ref = np.array([-1, 0, 11, 51, 54, 56, 52])
+    assert np.array_equal(test, ref), "test={} != ref={}".format(test, ref)
 
     logging.info("<< PASS : test_lookup_indices >>")
 

pisa/stages/absorption/pi_earth_absorption.py
@@ -7,6 +7,7 @@ using the same Layers module that is also used for oscillation.
 The survival probability is then calculated from the average cross-section
 with protons and neutrons.
 """
+
 from __future__ import absolute_import, print_function, division
 
 import numpy as np
@@ -16,12 +17,12 @@ from numba import guvectorize
 from pisa import FTYPE, TARGET
 from pisa import ureg
 from pisa.core.pi_stage import PiStage
-from pisa.utils.log import logging
 from pisa.utils.profiler import profile
 from pisa.stages.osc.layers import Layers
 from pisa.utils.numba_tools import WHERE
 from pisa.utils import vectorizer
 from pisa.utils.resources import find_resource
+
 
 FLAV_BAR_STR_MAPPING = {
     (0, -1): "e_bar",
@@ -35,12 +36,13 @@ FLAV_BAR_STR_MAPPING = {
 Mapping from flav and nubar container content to
 the string for this neutrino in the ROOT file.
 """
-        
+
+
 class pi_earth_absorption(PiStage):
     """
     earth absorption PISA Pi class
 
-    Paramaters
+    Parameters
     ----------
     earth_model : str
         PREM file path
@@ -50,37 +52,33 @@ class pi_earth_absorption(PiStage):
         detector depth
     prop_height : quantity (distance), optional
         height of neutrino production in the atmosphere
-    
-    Notes
-    -----
-    
+
     """
-    def __init__(self,
-                 earth_model,
-                 xsec_file,
-                 data=None,
-                 params=None,
-                 input_names=None,
-                 output_names=None,
-                 debug_mode=None,
-                 input_specs=None,
-                 calc_specs=None,
-                 output_specs=None,
-                 detector_depth=2.*ureg.km,
-                 prop_height=20.*ureg.km
-                ):
+    def __init__(
+        self,
+        earth_model,
+        xsec_file,
+        detector_depth=2.*ureg.km,
+        prop_height=20.*ureg.km,
+        data=None,
+        params=None,
+        input_names=None,
+        output_names=None,
+        debug_mode=None,
+        input_specs=None,
+        calc_specs=None,
+        output_specs=None,
+    ):
 
         expected_params = ()
         input_names = ()
         output_names = ()
 
-        input_apply_keys = ('weights',
-                           )
+        input_apply_keys = ('weights',)
+
         # The weights are simply scaled by the earth survival probability
-        output_calc_keys = ('survival_prob',
-                           )
-        output_apply_keys = ('weights',
-                            )
+        output_calc_keys = ('survival_prob',)
+        output_apply_keys = ('weights',)
 
         # init base class
         super().__init__(
@@ -110,17 +108,17 @@ class pi_earth_absorption(PiStage):
         self.prop_height = prop_height.m_as('km')
         # this does nothing for speed, but makes for convenient numpy style broadcasting
         # TODO: Use numba vectorization (not sure how that works with splines)
-        self.calculate_xsections = np.vectorize(self.calculate_xsections)  
-        
+        self.calculate_xsections = np.vectorize(self.calculate_xsections)
+
     def setup_function(self):
         import ROOT
         # setup the layers
         earth_model = find_resource(self.earth_model)
         self.layers = Layers(earth_model, self.detector_depth, self.prop_height)
-        # This is a bit hacky, but setting the electron density to 1. 
+        # This is a bit hacky, but setting the electron density to 1.
         # gives us the total density of matter, which is what we want.
         self.layers.setElecFrac(1., 1., 1.)
-        
+
         # setup cross-sections
         self.xsroot = ROOT.TFile(self.xsec_file)
         # set the correct data mode
@@ -142,7 +140,7 @@ class pi_earth_absorption(PiStage):
         # don't forget to un-link everything again
         self.data.unlink_containers()
 
-        # --- setup cross section and survival probability --- 
+        # --- setup cross section and survival probability ---
         if self.calc_mode == 'binned':
             # The cross-sections do not depend on nc/cc, so we can at least link those containers
             self.data.link_containers('nue', ['nue_cc', 'nue_nc'])
@@ -155,7 +153,7 @@ class pi_earth_absorption(PiStage):
             container['xsection'] = np.empty((container.size), dtype=FTYPE)
             container['survival_prob'] = np.empty((container.size), dtype=FTYPE)
         self.data.unlink_containers()
-    
+
     @profile
     def compute_function(self):
         # --- calculate the integrated density in the layers ---
@@ -173,7 +171,7 @@ class pi_earth_absorption(PiStage):
         # don't forget to un-link everything again
         self.data.unlink_containers()
 
-        # --- calculate survival probability --- 
+        # --- calculate survival probability ---
         if self.calc_mode == 'binned':
             # The cross-sections do not depend on nc/cc, so we can at least link those containers
             self.data.link_containers('nue', ['nue_cc', 'nue_nc'])
@@ -193,21 +191,22 @@ class pi_earth_absorption(PiStage):
                                   )
             container['survival_prob'].mark_changed(WHERE)
         self.data.unlink_containers()
-      
+
     @profile
     def apply_function(self):
         for container in self.data:
-            vectorizer.multiply(container['survival_prob'], out=container['weights'])
-            
+            vectorizer.imul(vals=container['survival_prob'], out=container['weights'])
+
     def calculate_xsections(self, flav, nubar, energy):
         '''Calculates the cross-sections on isoscalar targets.
-        The result is returned in cm^2. The xsection on one 
+        The result is returned in cm^2. The xsection on one
         target is calculated by taking the xsection for O16
-        and dividing it by 16. 
+        and dividing it by 16.
         '''
         flavor = FLAV_BAR_STR_MAPPING[(flav, nubar)]
         return (self.xsroot.Get('nu_'+flavor+'_O16').Get('tot_cc').Eval(energy)+
                 self.xsroot.Get('nu_'+flavor+'_O16').Get('tot_nc').Eval(energy))*10**(-38)/16. # this gives cm^2
+
 
 signatures = [
     '(f4[::1], f4[::1], f4[::1])',
@@ -220,7 +219,7 @@ signatures = [
 def calculate_integrated_rho(layer_dists, layer_densities, out):
     """Calculate density integrated over the path through all layers.
     Gives the length of a matter-equivalent water column in cm.
-    
+
     Parameters
     ----------
     layer_dists : vector
@@ -233,11 +232,12 @@ def calculate_integrated_rho(layer_dists, layer_densities, out):
     """
     out[0] = np.dot(layer_dists, layer_densities)*1e5 #distances are converted from km to cm
 
+
 @guvectorize(signatures, '(),()->()', target=TARGET)
 def calculate_survivalprob(int_rho, xsection, out):
     """Calculate survival probability given layer distances,
     layer densities and (pre-computed) cross-sections.
-    
+
     Parameters
     ----------
     int_rho : scalar

pisa/stages/aeff/pi_aeff.py
@@ -1,65 +1,70 @@
 """
 PISA pi stage to apply effective area weights
 """
+
 from __future__ import absolute_import, print_function, division
 
 from pisa.core.pi_stage import PiStage
-from pisa.utils.log import logging
-from pisa.utils.profiler import profile
 from pisa.utils import vectorizer
+from pisa.utils.profiler import profile
 
 
-class pi_aeff(PiStage):
+class pi_aeff(PiStage):  # pylint: disable=invalid-name
     """
     PISA Pi stage to apply aeff weights.
-    This combines the detector effective area with the flux weights calculated 
+
+    This combines the detector effective area with the flux weights calculated
     in an earlier stage to compute the weights.
-    Various scalings can be applied for particular event classes.
-    The weight is then multiplied by the livetime to get an event count.
 
-    Paramaters
+    Various scalings can be applied for particular event classes. The weight is
+    then multiplied by the livetime to get an event count.
+
+    Parameters
     ----------
+    data
+    params
+        Expected params are .. ::
 
-    livetime : Quantity with time units
+            livetime : Quantity with time units
+            aeff_scale : dimensionless Quantity
+            nutau_cc_norm : dimensionless Quantity
+            nutau_norm : dimensionless Quantity
+            nu_nc_norm : dimensionless Quantity
 
-    aeff_scale : dimensionless Quantity
-
-    nutau_cc_norm : dimensionless Quantity
-
-    nutau_norm : dimensionless Quantity
-
-    nu_nc_norm : dimensionless Quantity
-
-    Notes
-    -----
+    input_names
+    output_names
+    debug_mode
+    input_specs
+    calc_specs
+    output_specs
 
     """
-    def __init__(self,
-                 data=None,
-                 params=None,
-                 input_names=None,
-                 output_names=None,
-                 debug_mode=None,
-                 input_specs=None,
-                 calc_specs=None,
-                 output_specs=None,
-                ):
-
-        expected_params = ('livetime',
-                           'aeff_scale',
-                           'nutau_cc_norm',
-                           'nutau_norm',
-                           'nu_nc_norm',
-                          )
+    def __init__(
+        self,
+        data=None,
+        params=None,
+        input_names=None,
+        output_names=None,
+        debug_mode=None,
+        input_specs=None,
+        calc_specs=None,
+        output_specs=None,
+    ):
+        expected_params = (
+            'livetime',
+            'aeff_scale',
+            'nutau_cc_norm',
+            'nutau_norm',
+            'nu_nc_norm',
+        )
         input_names = ()
         output_names = ()
 
         # what are the keys used from the inputs during apply
-        input_apply_keys = ('weighted_aeff',
-                           )
+        input_apply_keys = ('weighted_aeff',)
+
         # what keys are added or altered for the outputs during apply
-        output_apply_keys = ('weights',
-                            )
+        output_apply_keys = ('weights',)
 
         # init base class
         super().__init__(
@@ -83,7 +88,6 @@ class pi_aeff(PiStage):
         # right now this stage has no calc mode, as it just applies scales
         # but it could if for example some smoothing will be performed!
 
-
     @profile
     def apply_function(self):
 
@@ -102,7 +106,9 @@ class pi_aeff(PiStage):
                 scale *= nutau_norm
             if 'nc' in container.name:
                 scale *= nu_nc_norm
-            vectorizer.multiply_and_scale(scale,
-                                          container['weighted_aeff'],
-                                          out=container['weights'],
-                                         )
+
+            vectorizer.imul_and_scale(
+                vals=container['weighted_aeff'],
+                scale=scale,
+                out=container['weights'],
+            )

pisa/stages/aeff/pi_weight.py
@@ -1,53 +1,51 @@
 """
 PISA pi stage to apply weights
 """
+
 from __future__ import absolute_import, print_function, division
 
 from pisa.core.pi_stage import PiStage
-from pisa.utils.log import logging
-from pisa.utils.profiler import profile
 from pisa.utils import vectorizer
+from pisa.utils.profiler import profile
 
 
-class pi_weight(PiStage):
+class pi_weight(PiStage):  # pylint: disable=invalid-name
     """
     PISA Pi stage to apply weights.
     This assumes a weight has already been calculated.
     The weight is then multiplied by the livetime to get an event count.
 
-    Paramaters
+    Parameters
     ----------
     params : ParamSet or sequence with which to instantiate a ParamSet.
-      Expected params are:
-        livetime : Quantity [time]
-          Detector livetime for scaling template 
-        weight_scale : Quantity [dimensionless]
-          Overall scaling/normalisation of template
+        Expected params are: .. ::
+
+            livetime : Quantity [time]
+                Detector livetime for scaling template
+            weight_scale : Quantity [dimensionless]
+                Overall scaling/normalisation of template
 
     """
-    def __init__(self,
-                 data=None,
-                 params=None,
-                 input_names=None,
-                 output_names=None,
-                 debug_mode=None,
-                 input_specs=None,
-                 calc_specs=None,
-                 output_specs=None,
-                ):
-
-        expected_params = ('livetime',
-                           'weight_scale',
-                          )
+    def __init__(
+        self,
+        data=None,
+        params=None,
+        input_names=None,
+        output_names=None,
+        debug_mode=None,
+        input_specs=None,
+        calc_specs=None,
+        output_specs=None,
+    ):
+        expected_params = ('livetime', 'weight_scale')
         input_names = ()
         output_names = ()
 
         # what are the keys used from the inputs during apply
-        input_apply_keys = ('weights',
-                           )
+        input_apply_keys = ('weights',)
+
         # what keys are added or altered for the outputs during apply
-        output_apply_keys = ('weights',
-                            )
+        output_apply_keys = ('weights',)
 
         # init base class
         super().__init__(
@@ -71,13 +69,15 @@ class pi_weight(PiStage):
         # right now this stage has no calc mode, as it just applies scales
         # but it could if for example some smoothing will be performed!
 
-
     @profile
     def apply_function(self):
-
         weight_scale = self.params.weight_scale.m_as('dimensionless')
         livetime_s = self.params.livetime.m_as('sec')
         scale = weight_scale * livetime_s
 
         for container in self.data:
-            vectorizer.scale(scale,container['weights'],out=container['weights'])
+            vectorizer.scale(
+                vals=container['weights'],
+                scale=scale,
+                out=container['weights'],
+            )

pisa/stages/background/atm_muons.py
@@ -26,23 +26,24 @@ class atm_muons(PiStage):
     Note that this stage only modifies an weights based on the systematics,
     it does not determine the nominal flux (this is assumed to either already
     available in the input files, or written by an upstream stage).
-    Paramaters
+
+    Parameters
     ----------
     params : ParamSet or instantiable thereto
-        Parameters for steering the stage. The following parameters must be included:
-        atm_muon_scale : quantity (dimensionless)
-            Normalisation of atmospheric muons
-        delta_gamma_mu_file : str
-            Path to file containing spectral index data
-        delta_gamma_mu_spline_kind : str
-            'kind' of spline, as per kwargs in scipy interp1d
-        delta_gamma_mu_variable : str
-            Name of variable in which the delta spectral index is splined
-            Currently only supported variable is 'coszen'
-        delta_gamma_mu : quantity (dimensionless)
-            Parameter controlling variation in spectral index
-    Notes
-    -----
+        Parameters for steering the stage. The following parameters must be included: .. ::
+
+            atm_muon_scale : quantity (dimensionless)
+                Normalisation of atmospheric muons
+            delta_gamma_mu_file : str
+                Path to file containing spectral index data
+            delta_gamma_mu_spline_kind : str
+                'kind' of spline, as per kwargs in scipy interp1d
+            delta_gamma_mu_variable : str
+                Name of variable in which the delta spectral index is splined
+                Currently only supported variable is 'coszen'
+            delta_gamma_mu : quantity (dimensionless)
+                Parameter controlling variation in spectral index
+
     """
 
     def __init__(self,

pisa/stages/data/csv_data_hist.py
@@ -9,7 +9,6 @@ import pandas as pd
 
 from pisa import FTYPE
 from pisa.core.pi_stage import PiStage
-from pisa.utils import vectorizer
 from pisa.utils.profiler import profile
 from pisa.core.container import Container
 from pisa.core.events_pi import EventsPi

commit 5f86d15f55827462267b1c5c5d1c1cc3a13ebb16

Bug fix that was introduced in #574

pisa/stages/osc/pi_nusquids.py
@@ -12,6 +12,7 @@ from __future__ import absolute_import, print_function, division
 import math
 import numpy as np
 from numba import guvectorize
+from scipy.interpolate import RectBivariateSpline
 
 from pisa import FTYPE, TARGET
 from pisa.core.pi_stage import PiStage