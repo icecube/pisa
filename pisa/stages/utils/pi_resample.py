c07b6b7116d124acd43380cd71290a0dffc2a41f
Alexander Trettin <alexander.trettin@physik.hu-berlin.de>
2020-05-02 20:05:47 +0200

    scale errors correctly, first working version

--- pisa/stages/utils/pi_resample.py
+++ pisa/stages/utils/pi_resample.py
@@ -25,15 +25,22 @@ class ResampleMode(Enum):
 
     UP = auto()
     DOWN = auto()
-
+    ARB = auto()
 
 class pi_resample(PiStage):  # pylint: disable=invalid-name
     """
-    Stage to resample events from one binning to another.
+    Stage to resample weighted MC histograms from one binning to another.
+    
+    Parameters
+    ----------
+    
+    scale_errors : bool, optional
+        If `True` (default), apply scaling to errors.
     """
 
     def __init__(
         self,
+        scale_errors=True,
         data=None,
         params=None,
         input_names=None,
@@ -54,7 +61,7 @@ class pi_resample(PiStage):  # pylint: disable=invalid-name
 
         # what are keys added or altered in the calculation used during apply
         assert calc_specs is None
-        if error_method in ["sumw2"]:
+        if scale_errors:
             output_apply_keys = ("weights", "errors")
         else:
             output_apply_keys = ("weights",)
@@ -77,10 +84,15 @@ class pi_resample(PiStage):  # pylint: disable=invalid-name
         # This stage only makes sense when going binned to binned.
         assert self.input_mode == "binned", "stage only takes binned input"
         assert self.output_mode == "binned", "stage only produces binned output"
-
+        
+        self.scale_errors = scale_errors
+        
         # The following tests whether `output_specs` is a strict up-sample
         # from `input_specs`, i.e. the bin edges of `output_specs` are a superset
         # of the bin edges of `input_specs`.
+        
+        # TODO: Test for ability to resample in two steps
+        # TODO: Update to new test nomenclature
         if input_specs.is_compat(output_specs):
             self.rs_mode = ResampleMode.UP
         elif output_specs.is_compat(input_specs):
@@ -88,9 +100,19 @@ class pi_resample(PiStage):  # pylint: disable=invalid-name
         else:
             raise ValueError("Binnings are not compatible with each other for resample")
 
-        # TODO: Implement downsampling.
+        # TODO: Implement downsampling
+        # TODO: Implement arbitrary resampling
         if self.rs_mode == ResampleMode.DOWN:
             raise NotImplementedError("Downsampling not yet implemented.")
+        if self.rs_mode == ResampleMode.ARB:
+            raise NotImplementedError("Arbitrary resampling not yet implemented.")
+
+    def setup_function(self):
+        # create the variables to be filled in `apply`
+        if self.scale_errors:
+            self.data.data_specs = self.input_specs
+            for container in self.data:
+                container["variances"] = np.empty((container.size), dtype=FTYPE)
 
     @profile
     def apply(self):
@@ -100,7 +122,14 @@ class pi_resample(PiStage):  # pylint: disable=invalid-name
         # We are overwriting the `apply` method rather than the `apply_function` method
         # because we are manipulating the data binning in a delicate way that doesn't
         # work with automatic rebinning.
-
+        if self.scale_errors:
+            self.data.data_specs = self.input_specs
+            for container in self.data:
+                vectorizer.pow(
+                    vals=container["errors"],
+                    pwr=2,
+                    out=container["variances"],
+                )
         self.data.data_specs = self.output_specs
         for container in self.data:
             # The built-in `binned_to_binned` method behaves as follows:
@@ -109,20 +138,21 @@ class pi_resample(PiStage):  # pylint: disable=invalid-name
             # - When a bin is split into smaller bins, each of the smaller bins gets
             #   the same value as the large bin.
             # This first step is the same whether we sample up or down.
-            container.binned_to_binned("weight", self.output_specs)
-            if self.error_method in ["sumw2"]:
-                container.binned_to_binned("weight_squared", self.output_specs)
+            container.binned_to_binned("weights", self.output_specs)
+            if self.scale_errors:
+                container.binned_to_binned("variances", self.output_specs)
+                container.binned_to_binned("errors", self.output_specs)
 
             # We now have to scale the weights and squared weights according to the bin
             # volumes depending on the sampling mode.
             if self.rs_mode == ResampleMode.UP:
                 # These are the volumes of the bins we sample *into*
                 upsampled_binvols = SmartArray(
-                    self.output_specs.bin_volumes(attach_units=False).ravel()
+                    self.output_specs.weighted_bin_volumes(attach_units=False).ravel()
                 )
                 # These are the volumes of the bins we sample *from*
                 coarse_volumes = SmartArray(
-                    self.input_specs.bin_volumes(attach_units=False).ravel()
+                    self.input_specs.weighted_bin_volumes(attach_units=False).ravel()
                 )
                 # For every upsampled bin, we need to know what the volume of the bin
                 # was where it came from. First, we get the position of the midpoint of
@@ -141,18 +171,18 @@ class pi_resample(PiStage):  # pylint: disable=invalid-name
                 )
                 # Finally, we scale the weights and squared weights by the ratio of the
                 # bin volumes in place:
-                vectorizer.imul(upsampled_binvols, container["weight"])
-                vectorizer.itruediv(origin_binvols, container["weight"])
-                container["weight"].mark_changed()
-                if self.error_method in ["sumw2"]:
-                    vectorizer.imul(upsampled_binvols, container["weight_squared"])
-                    vectorizer.itruediv(origin_binvols, container["weight_squared"])
-                    container["weight_squared"].mark_changed()
+                vectorizer.imul(upsampled_binvols, container["weights"])
+                vectorizer.itruediv(origin_binvols, container["weights"])
+                container["weights"].mark_changed()
+                if self.scale_errors:
+                    vectorizer.imul(upsampled_binvols, container["variances"])
+                    vectorizer.itruediv(origin_binvols, container["variances"])
+                    container["variances"].mark_changed()
             elif self.rs_mode == ResampleMode.DOWN:
                 pass  # not yet implemented
 
-            if self.error_method in ["sumw2"]:
+            if self.scale_errors:
                 vectorizer.sqrt(
-                    vals=container["weights_squared"], out=container["errors"]
+                    vals=container["variances"], out=container["errors"]
                 )
                 container["errors"].mark_changed()
