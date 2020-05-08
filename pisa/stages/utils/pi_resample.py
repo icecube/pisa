42fcb905d2bdd671e24760980a0d165e752ffe9c
Alexander Trettin <alexander.trettin@physik.hu-berlin.de>
2020-05-08 11:56:07 +0200

    remove dependence on automatic re-binning from resampling stage

--- pisa/stages/utils/pi_resample.py
+++ pisa/stages/utils/pi_resample.py
@@ -63,9 +63,13 @@ class pi_resample(PiStage):  # pylint: disable=invalid-name
         # what are keys added or altered in the calculation used during apply
         assert calc_specs is None
         if scale_errors:
-            output_apply_keys = ("weights", "errors")
+            output_apply_keys = ("weights_resampled", "errors_resampled")
         else:
-            output_apply_keys = ("weights",)
+            output_apply_keys = ("weights_resampled",)
+        
+        map_output_key = "weights_resampled"
+        map_output_error_key = "errors_resampled"
+        
         # init base class
         super().__init__(
             data=data,
@@ -80,6 +84,8 @@ class pi_resample(PiStage):  # pylint: disable=invalid-name
             output_specs=output_specs,
             input_apply_keys=input_apply_keys,
             output_apply_keys=output_apply_keys,
+            map_output_key=map_output_key,
+            map_output_error_key=map_output_error_key,
         )
 
         # This stage only makes sense when going binned to binned.
@@ -109,11 +115,17 @@ class pi_resample(PiStage):  # pylint: disable=invalid-name
             raise NotImplementedError("Arbitrary resampling not yet implemented.")
 
     def setup_function(self):
-        # create the variables to be filled in `apply`
-        if self.scale_errors:
-            self.data.data_specs = self.input_specs
-            for container in self.data:
-                container["variances"] = np.empty((container.size), dtype=FTYPE)
+        # Set up a container for intermediate storage of variances in input specs
+        self.data.data_specs = self.input_specs
+        for container in self.data:
+            container["variances"] = np.empty((container.size), dtype=FTYPE)
+        # set up containers for the resampled output in the output specs
+        self.data.data_specs = self.output_specs
+        for container in self.data:
+            container["weights_resampled"] = np.empty((container.size), dtype=FTYPE)
+            if self.scale_errors:
+                container["vars_resampled"] = np.empty((container.size), dtype=FTYPE)
+                container["errors_resampled"] = np.empty((container.size), dtype=FTYPE)
 
     @profile
     def apply(self):
@@ -123,68 +135,73 @@ class pi_resample(PiStage):  # pylint: disable=invalid-name
         # We are overwriting the `apply` method rather than the `apply_function` method
         # because we are manipulating the data binning in a delicate way that doesn't
         # work with automatic rebinning.
+
+        self.data.data_specs = self.input_specs
         if self.scale_errors:
-            self.data.data_specs = self.input_specs
             for container in self.data:
                 vectorizer.pow(
                     vals=container["errors"],
                     pwr=2,
                     out=container["variances"],
                 )
-        self.data.data_specs = self.output_specs
-        for container in self.data:
-            # The built-in `binned_to_binned` method behaves as follows:
-            # - When several bins are merged into one, the large bin contains the
-            #   average of the smaller bins.
-            # - When a bin is split into smaller bins, each of the smaller bins gets
-            #   the same value as the large bin.
-            # This first step is the same whether we sample up or down.
-            container.binned_to_binned("weights", self.output_specs)
-            if self.scale_errors:
-                container.binned_to_binned("variances", self.output_specs)
-                container.binned_to_binned("errors", self.output_specs)
 
-            # We now have to scale the weights and squared weights according to the bin
-            # volumes depending on the sampling mode.
+        input_binvols = SmartArray(
+            self.input_specs.weighted_bin_volumes(attach_units=False).ravel()
+        )
+        output_binvols = SmartArray(
+            self.output_specs.weighted_bin_volumes(attach_units=False).ravel()
+        )
+        
+        for container in self.data:
+            self.data.data_specs = self.input_specs
+            # we want these to be SmartArrays, so no `.get(WHERE)`
+            weights_flat_hist = container["weights"]
+            if self.scale_errors:
+                vars_flat_hist = container["variances"]
+            self.data.data_specs = self.output_specs
             if self.rs_mode == ResampleMode.UP:
-                # These are the volumes of the bins we sample *into*
-                upsampled_binvols = SmartArray(
-                    self.output_specs.weighted_bin_volumes(attach_units=False).ravel()
-                )
-                # These are the volumes of the bins we sample *from*
-                coarse_volumes = SmartArray(
-                    self.input_specs.weighted_bin_volumes(attach_units=False).ravel()
-                )
-                # For every upsampled bin, we need to know what the volume of the bin
-                # was where it came from. First, we get the position of the midpoint of
-                # each fine (output) bin:
+                # The `unroll_binning` function returns the midpoints of the bins in the
+                # dimension `name`.
                 fine_gridpoints = [
-                    # The `unroll_binning` function returns the midpoints of the bins
-                    # in the dimension `name`.
                     SmartArray(container.unroll_binning(name, self.output_specs))
                     for name in self.output_specs.names
                 ]
                 # We look up at which bin index of the input binning the midpoints of
-                # the output binning can be found, and assign to each the volume of the
+                # the output binning can be found, and assign to each the content of the
                 # bin of that index.
+                container["weights_resampled"] = translation.lookup(
+                    fine_gridpoints,
+                    weights_flat_hist,
+                    self.input_specs,
+                )
+                if self.scale_errors:
+                    container["vars_resampled"] = translation.lookup(
+                        fine_gridpoints,
+                        vars_flat_hist,
+                        self.input_specs,
+                    )
+                # These are the volumes of the bins we sample *from*
                 origin_binvols = translation.lookup(
-                    fine_gridpoints, coarse_volumes, self.input_specs
+                    fine_gridpoints,
+                    input_binvols,
+                    self.input_specs,
                 )
                 # Finally, we scale the weights and variances by the ratio of the
                 # bin volumes in place:
-                vectorizer.imul(upsampled_binvols, container["weights"])
-                vectorizer.itruediv(origin_binvols, container["weights"])
+                vectorizer.imul(output_binvols, container["weights_resampled"])
+                vectorizer.itruediv(origin_binvols, container["weights_resampled"])
                 if self.scale_errors:
-                    vectorizer.imul(upsampled_binvols, container["variances"])
-                    vectorizer.itruediv(origin_binvols, container["variances"])
+                    vectorizer.imul(output_binvols, container["vars_resampled"])
+                    vectorizer.itruediv(origin_binvols, container["vars_resampled"])
             elif self.rs_mode == ResampleMode.DOWN:
                 pass  # not yet implemented
 
             if self.scale_errors:
                 vectorizer.sqrt(
-                    vals=container["variances"], out=container["errors"]
+                    vals=container["vars_resampled"], out=container["errors_resampled"]
                 )
-
+    
+    
 def test_pi_resample():
     """Unit test for the resampling stage."""
     from pisa.core.distribution_maker import DistributionMaker
