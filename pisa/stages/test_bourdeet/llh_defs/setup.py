dd616166f0be9f6a094c86a2eb8cac091592283b
Etienne Bourbeau <27770178+bourdeet@users.noreply.github.com>
2020-06-11 18:05:13 +0200

    attempting to include c function compiling with cython

--- pisa/stages/test_bourdeet/llh_defs/setup.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-import numpy
-ext = Extension('poisson_gamma_mixtures', sources = ['poisson_gamma_mixtures.pyx', 'poisson_gamma.c'])
-setup(name="PG_MIXTURES", ext_modules = cythonize([ext]),include_dirs=[numpy.get_include()])
