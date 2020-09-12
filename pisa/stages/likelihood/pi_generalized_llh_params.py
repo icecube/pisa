ec05cb8a97ad56f9426e299ff772a5c64bcee108
Etienne Bourbeau <27770178+bourdeet@users.noreply.github.com>
2020-09-12 15:27:02 +0200

    renaming generalized poisson llh modules

--- pisa/stages/likelihood/pi_generalized_llh_params.py
+++ pisa/stages/likelihood/pi_generalized_llh_params.py
@@ -96,18 +96,18 @@ class pi_generalized_llh_params(PiStage):
 
 		# init base class
 		super(pi_generalized_llh_params, self).__init__(data=data,
-														 params=params,
-														 expected_params=expected_params,
-														 input_names=input_names,
-														 output_names=output_names,
-														 debug_mode=debug_mode,
-														 input_specs=input_specs,
-														 calc_specs=calc_specs,
-														 output_specs=output_specs,
-														 input_apply_keys=input_apply_keys,
-														 output_apply_keys=output_apply_keys,
-														 output_calc_keys=output_calc_keys,
-														 )
+												 params=params,
+												 expected_params=expected_params,
+												 input_names=input_names,
+												 output_names=output_names,
+												 debug_mode=debug_mode,
+												 input_specs=input_specs,
+												 calc_specs=calc_specs,
+												 output_specs=output_specs,
+												 input_apply_keys=input_apply_keys,
+												 output_apply_keys=output_apply_keys,
+												 output_calc_keys=output_calc_keys,
+												 )
 
 	def setup_function(self):
 		"""
