279db0b6e22f47d8d855a0234eec3b41842a665b
Maria <marialiubarska@gmail.com>
2025-06-17 13:45:31 -0500

    added init_test to charm stage

--- pisa/stages/xsec/correct_charm_y.py
+++ pisa/stages/xsec/correct_charm_y.py
@@ -150,4 +150,10 @@ class correct_charm_y(Stage):
                 modif_weights = container["weights"] * container["charm_y_distr_corr"]
                 container["weights"] = modif_weights
 
-                    
+    def init_test(**param_kwargs):
+    """Instantiation example that enables actual testing on toy containers from test_services.py"""
+        return correct_charm_y(
+            nu_cc_container_keys=["test1_cc"],
+            nubar_cc_container_keys=["test2_nc"]
+        )
+        
