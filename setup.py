a72f6ec8444ee9e29b1c0f243fcb8c9e5570aebb
T Ehrhardt <thehrh@users.noreply.github.com>
2026-06-10 18:24:49 +0200

    Remove upper python version cap in python_requires keyword to setuptools.setup() (#951)

--- setup.py
+++ setup.py
@@ -302,7 +302,7 @@ def do_setup():
         author_email='analysis@icecube.wisc.edu',
         url='https://github.com/icecubeopensource/pisa',
         cmdclass=cmdclasses,
-        python_requires='>=3.8,<3.15', # daemonflux 0.8.0 requires Py>=3.8
+        python_requires='>=3.8', # daemonflux 0.8.0 requires Py>=3.8
         setup_requires=SETUP_REQUIRES,
         install_requires=INSTALL_REQUIRES,
         extras_require=EXTRAS_REQUIRE,
