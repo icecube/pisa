ed3110a57d68a9416fa2bd89e7b4149c47265487
Weldert <jweldert@astro-06.Physik.Uni-Mainz.DE>
2026-06-12 11:16:07 +0200

    First (simple) implementation of ORCA energy scale

--- pisa/stages/utils/orca_scale.py
+++ pisa/stages/utils/orca_scale.py
@@ -1,8 +1,9 @@
 """
-PISA pi stage to apply effective area weights
+PISA stage to apply effective area weights and the energy scale systematic as defined by ORCA
 """
 
 from __future__ import absolute_import, print_function, division
+import numpy as np
 
 from pisa import ureg
 from pisa.core.param import Param, ParamSet
@@ -11,7 +12,25 @@ from pisa.utils.profiler import profile
 
 
 class orca_scale(Stage):  # pylint: disable=invalid-name
+    """
+    PISA stage to apply scaling systematics as defined by ORCA.
 
+    Parameters
+    ----------
+    params
+        Expected params are .. ::
+
+            hpt_norm : dimensionless Quantity
+            shower_norm : dimensionless Quantity
+            energy_scale : dimensionless Quantity
+
+        Expected container keys are .. ::
+
+            "weights"
+            "reco_energy"
+
+
+    """
     def __init__(
         self,
         **std_kwargs,
@@ -19,10 +38,12 @@ class orca_scale(Stage):  # pylint: disable=invalid-name
         expected_params = (
             'hpt_norm',
             'shower_norm',
+            'energy_scale',
         )
 
         expected_container_keys = (
             'weights',
+            'reco_energy',
         )
 
         # init base class
@@ -37,9 +58,25 @@ class orca_scale(Stage):  # pylint: disable=invalid-name
     def apply_function(self):
         hpt_norm = self.params.hpt_norm.m_as('dimensionless')
         shower_norm = self.params.shower_norm.m_as('dimensionless')
+        energy_scale = self.params.energy_scale.m_as('dimensionless')
+
+        ob = self.data['output_binning']
+        e_bins = ob.bin_edges[ob.names.index('reco_energy')].m
+        e_width = np.diff(np.log10(e_bins))
+        rel_loss = abs(np.log10(energy_scale)) / e_width
+        assert np.all(rel_loss < 1)
 
         for container in self.data:
             hist = container.get_hist('weights')[0]
             hist[:,:,0] *= hpt_norm
             hist[:,:,1] *= shower_norm
-            container.mark_changed('weights')
+
+            keep = (1-rel_loss)[:, np.newaxis, np.newaxis] * hist 
+            lose = rel_loss[:, np.newaxis, np.newaxis] * hist
+            if energy_scale > 1:
+                get = np.roll(lose, 1, axis=0)
+                get[0,:,:] = 0
+            else:
+                get = np.roll(lose, -1, axis=0)
+                get[-1,:,:] = 0
+            container['weights'] = keep + get
