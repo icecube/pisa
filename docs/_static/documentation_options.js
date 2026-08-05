b44bbcf4ac83763e06845a71c8a5659e846665c3
thehrh <thehrh@users.noreply.github.com>
2026-08-05 14:25:54 +0000

    deploy: 4f4b9a5efd60ebf3637485cdffb3d91fc2d3f8ad

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '4.3a1+10.g4f80b5f0.dirty',
+    VERSION: '4.3a1+11.g4f4b9a5e.dirty',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
